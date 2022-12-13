eb9ec3bdbe9efad0e7acde5651b65670a4fcb45b
bodV6 <fairybod@gmail.com>
2022-12-13 12:35:49 +0100

    Route Update

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
-function updateRoute(templateId) {
-  const template = document.getElementById(templateId);
+const routes = {
+    '/login': {templateId: 'login'},
+    '/dashboard': {templateId: 'dashboard'},
+};
+
+
+
+
+function updateRoute() {
+
+    const path = window.location.pathname;
+    const route = routes[path];
+
+    if (!route) {
+        return navigate('/login')
+    }
+
+  const template = document.getElementById(route.templateId);
   const view = template.content.cloneNode(true);
   const app = document.getElementById("app");
   app.innerHTML = "";
   app.appendChild(view);
 }
 
-updateRoute("dashboard");
+updateRoute();
+
+
+function navigate(path) {
+    window.history.pushState({}, path, path);
+    updateRoute();
+}
+
+
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href)
+}
+
+
+window.onpopstate = () => updateRoute();
+updateRoute();
+
+
+
+
+console.log(routes[window.location.pathname]);
+
