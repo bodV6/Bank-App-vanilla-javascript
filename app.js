20c825ffb3d9bc05e4ffd07b7e5f3ca2aaa270af
bodV6 <fairybod@gmail.com>
2022-12-14 15:47:34 +0100

    added credit route

--- app.js
+++ app.js
@@ -1,47 +1,43 @@
 const routes = {
-    '/login': {templateId: 'login'},
-    '/dashboard': {templateId: 'dashboard'},
+  "/login": { templateId: "login" },
+  "/dashboard": { templateId: "dashboard" },
+  "/credit": { templateId: "credit" },
 };
 
-
-
-
 function updateRoute() {
+  const path = window.location.pathname;
+  const route = routes[path];
 
-    const path = window.location.pathname;
-    const route = routes[path];
-
-    if (!route) {
-        return navigate('/login')
-    }
+  if (!route) {
+    return navigate("/login");
+  }
 
   const template = document.getElementById(route.templateId);
   const view = template.content.cloneNode(true);
   const app = document.getElementById("app");
   app.innerHTML = "";
   app.appendChild(view);
+
+  // Assignment
+
+  window.document.title = template.title;
+
+  // console.log(`${templateId}`);
 }
 
 updateRoute();
 
-
 function navigate(path) {
-    window.history.pushState({}, path, path);
-    updateRoute();
+  window.history.pushState({}, path, path);
+  updateRoute();
 }
-
 
 function onLinkClick(event) {
-    event.preventDefault();
-    navigate(event.target.href)
+  event.preventDefault();
+  navigate(event.target.href);
 }
-
 
 window.onpopstate = () => updateRoute();
 updateRoute();
 
-
-
-
-console.log(routes[window.location.pathname]);
-
+// console.log(routes[window.location.pathname]);
